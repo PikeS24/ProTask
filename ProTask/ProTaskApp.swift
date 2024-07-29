c176da4b7c88df89a2c9e6cd792746dc67375293
Pike Steck <haileysteck06@gmail.com>
2024-07-29 15:25:01 -0700

    Initial Commit

---
+++ ProTask/ProTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProTaskApp.swift
+//  ProTask
+//
+//  Created by Scholar on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ProTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
