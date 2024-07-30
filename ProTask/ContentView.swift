f483226ee8b53ec4bc3ed30656c5ffdfb8cd196e
Jada Martin <jadasmartin08@gmail.com>
2024-07-30 13:14:22 -0700

    he

--- ProTask/ContentView.swift
+++ ProTask/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Test")
                 HStack{
                     Text("This is the mental health tab")
                     Text("This is the APM tab")
