1196fe372266b7ef3cca83bd42e6c7d745580534
Briar Bryant <ceoofydying@gmail.com>
2024-07-30 11:36:47 -0700

    here is the second view

--- ProTask/ContentView.swift
+++ ProTask/ContentView.swift
@@ -3,22 +3,39 @@
 //  ProTask
 //
 //  Created by Scholar on 7/29/24.
-//
+//mental health and APM
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Bye, world!")
+        NavigationStack {
+            VStack {
+                HStack{
+                    Text("This is the mental health tab")
+                    Text("This is the APM tab")
+                    }
+                VStack{
+                    Text("This is the community tab")
+                        .padding(.top, 6.0)
+                    
+                }
         }
-        .padding()
+            .toolbar {
+                ToolbarItemGroup(placement: .status) {
+                    NavigationLink(destination: secondView()) {Text("Calendar")
+                        .foregroundColor(Color.red)}
+                    NavigationLink(destination: thirdview())
+                    {Text("Profile")}
+                                    
+                }
+            }
+        
+        }
     }
-}
+        }
 
+    
 #Preview {
     ContentView()
 }
---
+++ ProTask/thirdview.swift
@@ -0,0 +1,18 @@
+//
+//  thirdview.swift
+//  ProTask
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct thirdview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    thirdview()
+}
