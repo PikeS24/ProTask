523ee0d7ef59e9532888571ab0fc2eb965ffbf89
Briar Bryant <ceoofydying@gmail.com>
2024-07-30 13:14:50 -0700

    we commiting

---
+++ ProTask/timerView.swift
@@ -0,0 +1,18 @@
+//
+//  timerView.swift
+//  ProTask
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct timerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    timerView()
+}
